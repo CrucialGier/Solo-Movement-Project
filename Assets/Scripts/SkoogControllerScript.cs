42ba64dad87122a0d24b0ea0a9d4dca75a9c156d
Stewart Cole <stewart@>
2016-10-04 16:50:31 -0700

    add flexible code for jumping

--- Assets/Scripts/SkoogControllerScript.cs
+++ Assets/Scripts/SkoogControllerScript.cs
@@ -9,36 +9,47 @@ public class SkoogControllerScript : MonoBehaviour
     public float maxSpeed;
     public float idleDelayRange;
     public int differentIdles;
+    public float jumpForce;
+    public Transform groundCheck;
+    public LayerMask whatIsGround;
 
+    private Rigidbody2D rb2d;
     private bool isIdle;
     private float idleCount;
     private bool facingRight = false;
-    private Rigidbody2D rb2d;
+    private bool isGrounded = false;
+    private float groundRadius = 0.1f;
+
 
     Animator skoogAnimator;
 
-    void Start ()
+    void Start()
     {
         rb2d = gameObject.GetComponent<Rigidbody2D>();
         skoogAnimator = GetComponent<Animator>();
         idleCount = UnityEngine.Random.Range(3, idleDelayRange);
     }
 	
-	void FixedUpdate ()
+	void FixedUpdate()
     {
         Movement();
-        IdleCheck();
+        Idle();
+    }
+
+    void Update()
+    {
+        Jump();
     }
 
     void Movement()
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
-        skoogAnimator.SetFloat("Speed", Mathf.Abs(rb2d.velocity.magnitude));
-        Vector2 movement = new Vector2(moveHorizontal, rb2d.velocity.y);
+        skoogAnimator.SetFloat("groundSpeed", Mathf.Abs(rb2d.velocity.x));
+        Vector2 movement = new Vector2(moveHorizontal, 0);
 
-        if (rb2d.velocity.magnitude < maxSpeed)
+        if (Mathf.Abs(rb2d.velocity.x) < maxSpeed)
         {
-            rb2d.AddForce(movement * speed);
+            rb2d.AddForce(movement * speed, 0);
         }
 
         if (moveHorizontal > 0 && !facingRight)
@@ -51,6 +62,18 @@ public class SkoogControllerScript : MonoBehaviour
         }
     }
 
+    void Jump()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
+        skoogAnimator.SetBool("OnGround", isGrounded);
+        skoogAnimator.SetFloat("vSpeed", rb2d.velocity.y);
+        if (isGrounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            skoogAnimator.SetBool("OnGround", false);
+            rb2d.AddForce(new Vector2(0, jumpForce));
+        }
+    }
+
     void Flip()
     { 
         facingRight = !facingRight;
@@ -59,31 +82,20 @@ public class SkoogControllerScript : MonoBehaviour
         transform.localScale = direction;
     }
 
-    void IdleCheck()
+    void Idle()
     {
-        if (rb2d.velocity.magnitude == 0)
+        if (rb2d.velocity == new Vector2(0, 0))
         {
-            isIdle = true;
-            IdleTimer();
-        }
-        else
-        {
-            isIdle = false;
-        }
-    }
-
-    void IdleTimer()
-    {
-        idleCount -= Time.deltaTime;
-        if (idleCount <= 0)
-        {
-            skoogAnimator.SetInteger("Idle", 1);
-            Debug.Log(skoogAnimator.GetInteger("Idle"));
-            idleCount = UnityEngine.Random.Range(12, idleDelayRange);
-        }
-        else
-        {
-            skoogAnimator.SetInteger("Idle", 0);
+            idleCount -= Time.deltaTime;
+            if (idleCount <= 0)
+            {
+                skoogAnimator.SetInteger("Idle", 1);
+                idleCount = UnityEngine.Random.Range(12, idleDelayRange);
+            }
+            else
+            {
+                skoogAnimator.SetInteger("Idle", 0);
+            }
         }
     }
 }
